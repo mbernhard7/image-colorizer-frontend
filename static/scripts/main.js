072ca8fc20c6ea563adfc5aea57e7a52c9d15b0a
Miles Bernhard <mbernhard22@students.claremontmckenna.edu>
2022-01-09 00:02:15 -0500

    fix

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,7 +1,7 @@
 var imageName;
 var newImageName;
 var fileExtension;
-apiURL = "https://www.colorizer-backend.milesbernhard.com";
+apiURL = "http://www.colorizer-backend.milesbernhard.com";
 if (window.location.href == 'http://127.0.0.1:8000/' || window.location.href == 'http://localhost:8000/' ) {
     apiURL = "http://127.0.0.1:5000"
 }
